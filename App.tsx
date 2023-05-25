2eef84e1e0f74eb4c9112cb105eb7a341bda6ca5
k4k4k4k <99814691+k4k4k4k@users.noreply.github.com>
2023-05-25 15:47:48 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './style.css';
+import { useState, useEffect, useRef } from 'react';
+
+export default function App() {
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false)
+  const mins = Math.floor(time / 60);
+  const secs = time % 60;
+  const int = useRef(null)
+  
+  const startTimer = () => {
+    setIsActive(true)
+  };
+  const stopTimer = () => {
+    setIsActive(false)
+  };
+  const resetTimer = () => {
+    setTime(0)
+  };
+
+  useEffect(()=>{
+    if (isActive)int.current = setInterval(()=>setTime(prev=>prev+1), 1000)
+    else {
+      clearInterval(int.current)
+    }
+    return () => clearInterval(int.current)}, [isActive])
+
+  return (
+    <div className="container">
+      <h1>Timer</h1>
+      <span>{mins}:</span>
+      <span>{secs}</span>
+      <div>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-2nszkw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-2nszkw)
